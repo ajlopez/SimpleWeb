6cee306074e9ed42b1602c6d4f1c0158275168dd
ajlopez <webmaster@ajlopez.com>
2012-12-31 17:19:24 -0300

    New top menu in customers sample

--- samples/customers/server.js
+++ samples/customers/server.js
@@ -11,6 +11,14 @@ var customers = [
 
 var maxid = 3;
 
+function getCustomerById(id) {
+    for (var n in customers) {
+        var customer = customers[n];
+        if (customer.id === id)
+            return customer;
+    }
+}
+
 function doHeader(res, title)
 {
     res.writeHead(200, {'content-type': 'text/html'});
@@ -18,14 +26,29 @@ function doHeader(res, title)
     res.write('CRUD Simple Sample: ' + title);
     res.write('</title>');
     res.write('<link rel="stylesheet" href="/css/bootstrap.css">');
+    res.write('<link rel="stylesheet" href="/css/styles.css">');
     res.write('</head>');
     res.write('<body>');
-    
-    res.write('<div>');
-    res.write('<a href="/">Home</a> ');
-    res.write('<a href="/customer">Customers</a>');
-    res.write('</div>');
-    
+
+    res.write('<div class="navbar">\n');
+    res.write('<div class="navbar-inner">\n');
+    res.write('<div class="container">\n');
+    res.write('<div class="nav">\n');
+    res.write('<ul class="nav">\n');
+
+    res.write('<li>\n');
+    res.write('<a href="/">Home</a>\n');
+    res.write('</li>\n');
+    res.write('<li>\n');
+    res.write('<a href="/customer">Customers</a>\n');
+    res.write('</li>\n');
+
+    res.write('</ul>\n');
+    res.write('</div>\n');
+    res.write('</div>\n');
+    res.write('</div>\n');
+    res.write('</div>\n');
+        
     res.write('<h1>');
     res.write(title);
     res.write('</h1>');
@@ -54,19 +77,30 @@ function doTBD(res)
 
 function doCustomerView(req, res)
 {
+    var id = parseInt(req.query.id);
+    var customer = getCustomerById(id);
     doHeader(res, 'Customer');
-    doTBD(res);
+    res.write('<div class="btn-group actions">\n');
+    res.write('<a class="btn btn-info" href="/customer">Customers</a>\n');
+    res.write('<a class="btn btn-primary" href="/customer/update?id=' + id + '">Update</a>\n');
+    res.write('<a class="btn btn-danger" href="/customer/delete?id=' + id + '">Delete</a>\n');
+    res.write('</div>\n');
+    res.write('<table class="table-striped table-bordered view">\n');
+    res.write('<tr><td>Id</td><td>' + id + '</td></tr>\n');
+    res.write('<tr><td>Name</td><td>' + customer.name + '</td></tr>\n');
+    res.write('</table>\n');
+    doFooter(res);
     res.end();
 }
 
 function doCustomerList(req, res)
 {
     doHeader(res, 'Customers');
-    res.write('<div class="btn-group">\
+    res.write('<div class="btn-group actions">\
 <a class="btn btn-primary" href="/customer/new">New Customer</a>\
 </div>');
     
-    res.write('<table class="table-striped table-bordered" style="min-width: 500px;">\n');
+    res.write('<table class="table-striped table-bordered list">\n');
     res.write('<tr><th>Id</th><th>Name</th></tr>\n');
     
     customers.forEach(function(customer) {
@@ -86,7 +120,7 @@ function doCustomerNew(req, res)
 {
     doHeader(res, 'New Customer');
     
-    res.write('<form action="/customer/newprocess" method="get">\n');
+    res.write('<form action="/customer/newprocess" method="post">\n');
     res.write('<fieldset>\n');
     res.write('<legend>Name</legend>\n');
     res.write('<div><input name="name"></div>\n');
@@ -103,7 +137,7 @@ function doCustomerNew(req, res)
 function doCustomerNewProcess(req, res)
 {
     maxid++;
-    var customer = { id: maxid, name:  req.query.name };
+    var customer = { id: maxid, name:  req.body.name };
     customers.push(customer);
     res.writeHead(302, { 'Location': '/customer' });
     res.end();
