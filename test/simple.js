7366521ceaac4b0a8faf4e6bfd99438af72daed0
ajlopez <webmaster@ajlopez.com>
2012-12-31 13:56:51 -0300

    Refactor tests to use nodeunit

--- test/simple.js
+++ test/simple.js
@@ -1,21 +1,32 @@
 
-var simpleweb = require('..'),
-    assert = require('assert');
+var simpleweb = require('..');
 
-assert.ok(simpleweb);
-assert.equal(typeof simpleweb, 'function');
+exports['simpleweb is a function'] = function(test) {
+    test.ok(simpleweb);
+    test.equal(typeof simpleweb, 'function');
+    test.done();
+}
 
-// get application
+exports['get application'] = function(test) {
+    var app = simpleweb();
+    test.ok(app);
+    test.equal(typeof app, 'function');
+    test.equal(app.length, 2);
+    test.done();
+}
 
-var app = simpleweb();
-assert.ok(app);
-assert.equal(typeof app, 'function');
-assert.equal(app.length, 2);
+exports['use is a function'] = function(test) {
+    var app = simpleweb();
+    test.ok(app.use);
+    test.equal(typeof app.use, 'function');
+    test.done();
+}
 
-// use function
-
-assert.ok(app.use);
-assert.equal(typeof app.use, 'function');
-
-// register and call middlewarevar a = 0;function setA(req, res, next) {    a = 1;    next();};
-app.use(setA);app();assert.ok(a, 1);// register and call two middlewaresvar a = 0;function set1(req, res, next) {    a = 1;    next();};function set2(req, res, next) {    a *= 2;    next();};app.use(set1);app.use(set2);app();assert.ok(a, 2);
+exports['register and call a middleware'] = function(test) {
+    var a = 0;    function setA(req, res, next) {        a = 1;        next();    };
+    var app = simpleweb();
+    app.use(setA);    app();    test.ok(a, 1);
+    test.done();
+}exports['register and call two middlewares'] = function(test) {    var a = 0;    function set1(req, res, next) {        a = 1;        next();    };    function set2(req, res, next) {        a *= 2;        next();    };    var app = simpleweb();
+    app.use(set1);    app.use(set2);    app();    test.ok(a, 2);
+    test.done();}
