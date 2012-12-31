66fde1beab73e5b011736a7e4fc5bff31e515665
ajlopez <webmaster@ajlopez.com>
2012-12-31 12:49:04 -0300

    Query middleware, first test

---
+++ test/query.js
@@ -0,0 +1,5 @@
+
+var simpleweb = require('..'),
+    assert = require('assert');
+assert.ok(simpleweb.query);assert.equal(typeof simpleweb.query, 'function');var query = simpleweb.query();
+assert.ok(query);
