174db878e7fda184067d9d05bae35fb5b62e1881
ajlopez <webmaster@ajlopez.com>
2012-12-31 13:03:47 -0300

    Use query, with test

--- test/query.js
+++ test/query.js
@@ -2,4 +2,4 @@
 var simpleweb = require('..'),
     assert = require('assert');
 assert.ok(simpleweb.query);assert.equal(typeof simpleweb.query, 'function');var query = simpleweb.query();
-assert.ok(query);
+assert.ok(query);// invoke with urlvar req = { url: '/page?name=foo' };query(req,null,function() {});assert.ok(req.query);assert.ok(req.query.name);assert.equal(req.query.name, 'foo');// use and invokevar app = simpleweb();app.use(simpleweb.query());var req = { url: '/page?name=foo&data=bar' };app(req, null);assert.ok(req.query);assert.ok(req.query.name);assert.equal(req.query.name, 'foo');assert.ok(req.query.data);assert.equal(req.query.data, 'bar');
