a732301aa7d961a414ecac3e8dd70db037750c1a
ajlopez <webmaster@ajlopez.com>
2012-12-31 11:05:35 -0300

    Use in application, first test

--- test/simple.js
+++ test/simple.js
@@ -1,1 +1,20 @@
-var simpleweb = require('..'),    assert = require('assert');assert.ok(simpleweb);assert.equal(typeof simpleweb, 'function');
+
+var simpleweb = require('..'),
+    assert = require('assert');
+
+assert.ok(simpleweb);
+assert.equal(typeof simpleweb, 'function');
+
+// get application
+
+var app = simpleweb();
+assert.ok(app);
+assert.equal(typeof app, 'function');
+assert.equal(app.length, 2);
+
+// use function
+
+assert.ok(app.use);
+assert.equal(typeof app.use, 'function');
+
+
